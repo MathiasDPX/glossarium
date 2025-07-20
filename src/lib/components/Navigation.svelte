b7740ee1f0ca0a04dd0fe9dd0c5d5fa62a1dab9b
MathiasDPX <mathias@dupeux.net>
2025-07-21 00:18:38 +0200

    fix: same margin everywhere for navbar's items

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -2,9 +2,9 @@
 export let items: { id: string; label: string }[] = [];
 </script>
 
-<ul class="flex space-x-4 text-sm text-yellow-900">
-    {#each items as item}
-    <li>
+<ul class="flex text-sm text-yellow-900">
+    {#each items as item (item.id)}
+    <li class={items.indexOf(item) === 0 ? "" : "ml-4"}>
         <a href="#{item.id}" class="hover:underline transition-all duration-200 hover:text-yellow-700">
             {item.label}
         </a>
